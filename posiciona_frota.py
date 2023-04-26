ba5c15ec65a01c0443a817c9cc63631cd9dc9ad4
cauepda <cauepda@al.insper.edu.br>
2023-04-26 08:44:33 -0300

    construção do "posição frota"

---
+++ posiciona_frota.py
@@ -0,0 +1,16 @@
+def posiciona_frota(infos_navios):
+    tabuleiro = []
+
+    for j in range(0, 10):
+        lista_adicional = []
+        
+        for i in range(0, 10):
+            lista_adicional.append(0)
+
+        tabuleiro.append(lista_adicional)
+
+    for posicoes in infos_navios.values():
+        for posicao in posicoes:
+            for posicao_exata in posicao:
+                tabuleiro[posicao_exata[0]][posicao_exata[1]] = 1
+    return tabuleiro
